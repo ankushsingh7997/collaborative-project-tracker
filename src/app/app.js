130c9f4ab07426185effac3c46a2c593c12644ef
ankushsingh7997 <cu.16bcs2432@gmail.com>
2025-08-24 00:16:46 +0530

    Cors allowed

--- src/app/app.js
+++ src/app/app.js
@@ -41,14 +41,15 @@ app.set("trust proxy", true);
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+app.use(cors());
 
 // CORS configuration
-app.use(cors({
-    origin: process.env.NODE_ENV === 'producrtrtion' 
-        ? process.env.CLIENT_URL 
-        : ["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:3000", "http://127.0.0.1:5173"],
-    credentials: true
-}));
+// app.use(cors({
+//     origin: process.env.NODE_ENV === 'producrtrtion' 
+//         ? process.env.CLIENT_URL 
+//         : ["http://localhost:3000", "http://localhost:5173", "http://127.0.0.1:3000", "http://127.0.0.1:5173"],
+//     credentials: true
+// }));
 
 app.use(hpp());
 app.use(xss());
